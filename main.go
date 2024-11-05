8c03342bc7c2f40dd2813c59b865313b9d919dc3
denimarswnadi <denimarswandi@gmail.com>
2024-11-05 13:14:19 +0800

    + sync

--- main.go
+++ main.go
@@ -11,22 +11,22 @@ import (
 
 func main() {
 	util.LoadEnv()
-	// Database credentials
-	dbName := os.Getenv("DB_NAME")         // Replace with your database name
-	dbUser := os.Getenv("DB_USER")         // Replace with your MySQL username
-	dbPassword := os.Getenv("DB_PASSWORD") // Replace with your MySQL password
+	// database credentials load on env
+	dbName := os.Getenv("DB_NAME")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
 
-	// Create a timestamp in the format sipaham<year><month><date><hour><minute>
+	// create a timestamp in the format sipaham<year><month><date><hour><minute>
 	timestamp := time.Now().Format("200601021504") // YYYYMMDDHHMM
 	backupFilename := fmt.Sprintf("sipaham%s.sql", timestamp)
 
-	// Set the MYSQL_PWD environment variable to securely pass the password
+	// set the MYSQL_PWD environment variable to securely pass the password
 	os.Setenv("MYSQL_PWD", dbPassword)
 
-	// Construct the mysqldump command without the password flag
+	// construct the mysqldump command without the password flag
 	cmd := exec.Command("mysqldump", "-u"+dbUser, dbName)
 
-	// Create the backup file
+	// create the backup file
 	backupFile, err := os.Create(backupFilename)
 	if err != nil {
 		fmt.Println("Error creating backup file:", err)
@@ -34,18 +34,18 @@ func main() {
 	}
 	defer backupFile.Close()
 
-	// Set the command's output to the backup file
+	// set the command's output to the backup file
 	cmd.Stdout = backupFile
 
-	// Capture only stderr
+	// capture only stderr
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
 
-	// Run the command
+	// run the command
 	err = cmd.Run()
 	if err != nil {
 		fmt.Println("Error creating backup:", err)
-		fmt.Printf("Details: %s\n", stderr.String()) // Print stderr output if any
+		fmt.Printf("Details: %s\n", stderr.String())
 		return
 	}
 
